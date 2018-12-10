4362966da3f1895fe12038e39e2cfe70200daeea
Chua Chee Wee Paul <chuacheewee77@gmail.com>
2018-12-10 17:01:27 +0800

    Testing For Uploading

---
+++ WafflesCollege_Group7/src/main/java/com/example/demo/WafflesCollegeGroup7Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WafflesCollegeGroup7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WafflesCollegeGroup7Application.class, args);
+	}
+}
